4a5890d37cec8a69161f2f67cc54fcdeb7c05bfa
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 15:35:42 +0100

    first project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Naprawa samochodu",
+      amount: 250.54,
+      date: new Date(2023, 2, 8),
+    },
+    {
+      id: "e2",
+      title: "Paliwo",
+      amount: 100.01,
+      date: new Date(2023, 2, 4),
+    },
+    {
+      id: "e3",
+      title: "Zakupy",
+      amount: 58.37,
+      date: new Date(2023, 2, 6),
+    },
+  ];
+
+  return (
+    <div>
+      <h1></h1>
+      <Expenses items={expenses}/>
+    </div>
+  );
+}
+
+export default App;
