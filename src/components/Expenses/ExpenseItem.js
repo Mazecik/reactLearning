84a96924435e5222db608020a7320f25420920a7
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 16:02:45 +0100

    clickHandler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+
+    let title = props.title;
+
+    const clickHandler = () => {
+        title = 'CHANGED';
+    };
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount} PLN</div>
+                <button onClick={clickHandler}>Change Title</button>
             </div>
         </Card>
     );
-}
+};
 
 export default ExpenseItem;
