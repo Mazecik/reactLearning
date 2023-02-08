1782bfa2d322772373993a32bd975164c075e569
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 18:21:25 +0100

    Expense filter + new Expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+    const [filterYear, setFilterYear] = useState('2023');
+
+    // const filterChangeHandler = (value) => {
+    //     setFilterYear(value);
+    //     console.log(value);
+    // }
+
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filterYear} setFilterChange={setFilterYear}/>
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
--- src/components/Expenses/NewExpense/NewExpense.js
+++ src/components/Expenses/NewExpense/NewExpense.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import ExpenseDate from "../ExpenseDate";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const SaveExpenseData = (enteredExpensData) => {
+        const expanseData = {
+            ...enteredExpensData,
+            id: Math.random().toString()
+        };
+        props.onAddExpanse(expanseData);
+        // console.log(expanseData);
+    }
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={SaveExpenseData} />
         </div>
     );
 };
