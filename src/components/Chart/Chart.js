227b3af1bbf83139614579a6d8f1bd151005cb1a
BuildTools <unconfigured@null.spigotmc.org>
2023-02-16 06:04:36 +0100

    Początek budowania wykresu wydatków.

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+    let values = props.data.map((item) => {
+        return item.value; 
+    });
+    let maxVal = Math.max(values);
+    return (
+        <div className="chart">
+            {props.data.map((item) => {
+                <ChartBar
+                    key={item.label}
+                    value={item.value}
+                    maxValue={maxVal}
+                    label={item.label}
+                />;
+            })}
+        </div>
+    );
+};
+
+export default Chart;
